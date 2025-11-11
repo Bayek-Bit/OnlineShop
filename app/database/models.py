e4964e8cfb4d56306d8c442d8eca2d880eeead37
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2025-11-11 18:50:20 +0500

    Models changed. Now working relationships

--- app/database/models.py
+++ app/database/models.py
@@ -34,7 +34,7 @@ class Item(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
-    category: Mapped["Category"] = relationship("Category", back_populates="category")
+    category: Mapped["Category"] = relationship("Category", back_populates="items")
     name: Mapped[str] = mapped_column(String(25), nullable=False)
     description: Mapped[str] = mapped_column(String(512), nullable=True)
     price: Mapped[int] = mapped_column(Integer, nullable=False)
@@ -44,12 +44,13 @@ class Order(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped[int] = relationship("User", foreign_keys=[user_id])
     total_sum: Mapped[int] = mapped_column(Integer, nullable=False)
     status: Mapped[str] = mapped_column(String(25), default="pending_payment", nullable=False)
     executor_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
-    executor: Mapped["User"] = relationship(ForeignKey("user.id"))
+    executor: Mapped["User"] = relationship("User", foreign_keys=[executor_id])
     # время создания
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(), nullable=False)
     # дедлайн оплаты
     expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
