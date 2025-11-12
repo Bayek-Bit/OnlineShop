f7cf6cabda13806ae3052a2e90f584b46aa4d278
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2025-11-12 22:48:30 +0500

    Add new settings: payment timeout and order status

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -4,6 +4,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 CART_TTL = 3600
 # Время истечения срока хранения товаров в категориях
 ITEMS_TTL = 3600
+# Время на оплату заказа
+PAYMENT_TIMEOUT = 600
+
+# Константы статусов заказов
+ORDER_STATUS_PENDING_PAYMENT = "pending_payment"
+ORDER_STATUS_AWAITING_CONFIRMATION = "awaiting_executor_confirmation"
+ORDER_STATUS_IN_PROGRESS = "in_progress"
+ORDER_STATUS_COMPLETED = "completed"
+ORDER_STATUS_CANCELLED = "cancelled"
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
